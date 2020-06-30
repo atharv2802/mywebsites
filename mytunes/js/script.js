b7e2c41b0293bd0ba69a83fb0f8de7f96ec3697c
Atharv Sunil Biradar <padmasinha2802@gmail.com>
2020-06-30 18:07:24 +0530

    My Sites and Apps

---
+++ mytunes/js/script.js
@@ -0,0 +1,48 @@
+// Responsive nav
+
+$(function() {
+  menu = $('nav ul');
+
+  $('#openup').on('click', function(e) {
+    e.preventDefault();
+    menu.slideToggle();
+    });
+
+  $(window).resize(function() {
+    var width = $(this).width();
+    if (w > 480 && menu.is(':hidden')) {
+      menu.removeAttr('style');
+    }
+    });
+
+  $('nav li').on('click', function(e) {
+    var w = $(window).width();
+    if (w < 480) {
+      menu.slideToggle();
+    }
+  });
+  $('.open-menu').height($(window).height());
+});
+
+// Smooth Scrolling
+
+// Smooth Scrolling
+$('.cf a').on('click', function(event) {
+  if (this.hash !== '') {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top
+      },
+      800,
+      function() {
+        window.location.hash = hash;
+      }
+    );
+  }
+});
+
+
