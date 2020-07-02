c538bcd565ab1ea0d991207ca12befb45ab64221
Atharv Sunil Biradar <padmasinha2802@gmail.com>
2020-07-02 14:28:34 +0530

    My Sites and Apps

---
+++ shuttershot/js/scripts.js
@@ -0,0 +1,17 @@
+$('.landing-text a').on('click', function(event) {
+  if (this.hash !== '') {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top
+      },
+      2000,
+      function() {
+        window.location.hash = hash;
+      }
+    );
+  }
+});
