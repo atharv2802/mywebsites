91e85823ecc1d480a7c78dbf2dc3d3d358481ba2
Atharv Sunil Biradar <padmasinha2802@gmail.com>
2020-07-18 23:20:18 +0530

    My Sites and Apps

--- statecap/js/main.js
+++ statecap/js/main.js
@@ -3,7 +3,7 @@ const matchList = document.getElementById('match-list');
 
 // Search states.json and filter it
 const searchStates = async searchText => {
-    const res = await fetch('../data/states.json');
+    const res = await fetch('data/states.json');
     const states = await res.json();
     
     // get matches to cuurent text input
